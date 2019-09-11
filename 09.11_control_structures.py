4355244e1709c4b8792e7aca6b73461e3221afdd
SaraPena <pena.e.sara@gmail.com>
2019-09-11 15:46:11 -0500

    Add bayes-python-exercises sandbox & notes

---
+++ 09.11_control_structures.py
@@ -0,0 +1,14 @@
+i_like_coffee = True
+
+if i_like_coffee:
+    print('I like coffee!')
+    print('Coffee is the best!')
+
+it_is_raining = True
+
+if it_is_raining:
+    print("Better bring an umbrella!")
+else:
+    print("Looks like a nice, sunny day!")
+
+
